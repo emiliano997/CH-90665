d4394f14972c1e711673b0d2b9cca943323a2973
emilianoperez <emi.perez997@gmail.com>
2025-05-20 20:30:29 -0300

    [ADD] Clase 2

--- ecommerce/src/app/layout.js
+++ ecommerce/src/app/layout.js
@@ -26,8 +26,6 @@ export default function RootLayout({ children }) {
         <Navbar />
 
         {children}
-
-        <script src="https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.js"></script>
       </body>
     </html>
   );
